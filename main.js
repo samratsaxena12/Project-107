7a2e5e9050783e5227ea28ace8c373b9dea207ed
samratsaxena12 <102014414+samratsaxena12@users.noreply.github.com>
2022-03-25 17:36:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/yeA1raLXA/model.json', modelLoaded)
+}
+
+function modelLoaded() {
+    classifier.classify(gotResult);
+}
